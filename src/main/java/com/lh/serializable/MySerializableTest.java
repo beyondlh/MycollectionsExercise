7d434cd1160de89a13392f5632d194761c5f4dad
lh <beyondlh@126.com>
2016-11-15 09:20:57 +0800

    反序列化过程中final修饰的直接量会重新计算值

--- src/main/java/com/lh/serializable/MySerializableTest.java
+++ src/main/java/com/lh/serializable/MySerializableTest.java
@@ -2,16 +2,18 @@ package com.lh.serializable;
 
 import com.lh.Apple;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 /**
  * Created by lh on 2016/11/2.
  */
 public class MySerializableTest {
     public static void main(String[] args) {
-        Apple apple = new Apple(2);
+/*        Apple apple = new Apple(2);
 
-        /*只序列化属性*/
+        *//*只序列化属性*//*
         try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("D:\\Apple.txt"))) {
             objectOutputStream.writeUTF(String.valueOf(apple.getWeight()));
         } catch (IOException e) {
@@ -22,11 +24,11 @@ public class MySerializableTest {
             objectOutputStream.writeObject(apple);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
         try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("D:\\Apple.txt"))) {
             Apple a = (Apple) objectInputStream.readObject();
-            System.out.print(a.getWeight());
+            System.out.print(a.getBird().num);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
